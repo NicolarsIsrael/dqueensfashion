52385fb537665ff7a284dd0ef07afe971fe39f50
NicolarsIsrael <bakareisrael@gmail.com>
2020-05-22 08:46:59 +0100

    web project added

---
+++ DQueensFashion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DQueensFashion.Startup))]
+namespace DQueensFashion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
