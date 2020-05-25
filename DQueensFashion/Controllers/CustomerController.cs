cabe14cc5a6d7ebf88354da9811996862ddb2d00
NicolarsIsrael <bakareisrael@gmail.com>
2020-05-25 18:58:15 +0100

    customer dashboard added

---
+++ DQueensFashion/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using DQueensFashion.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DQueensFashion.Controllers
+{
+    [Authorize(Roles=AppConstant.CustomerRole)]
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return RedirectToAction(nameof(Dashboard));
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
