d9a3fa1837e6d3155bf26ac05aefa38c0ed7a47d
NicolarsIsrael <bakareisrael@gmail.com>
2020-09-11 07:35:38 +0100

    change background

--- DQueensFashion.Utilities/AppConstant.cs
+++ DQueensFashion.Utilities/AppConstant.cs
@@ -136,7 +136,7 @@ namespace DQueensFashion.Utilities
 
         public static readonly string[,] faqShippingReturn = {
             { "Do you ship internationally?",
-                "Yes, Canada and Europe." },
+                "Yes, we ship to USA, Canada and Europe." },
 
             { "Shipping information.",
                 "Kindly fill in your correct address including zip code and your full name " },
